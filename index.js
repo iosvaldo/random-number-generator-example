9096a5110d080643f89b13d873d62e597c17872b
iosvaldo <35927834+iosvaldo@users.noreply.github.com>
2021-06-14 02:52:00 -0400

    second iteration

--- index.js
+++ index.js
@@ -44,18 +44,30 @@
 
 //////////////////////////////// The Love Calculator /////////////////////////////////////////////////
 
+
 prompt("What is your name?");
 prompt("What is their name?");
 
 var lovescore = Math.random() * 100;
 lovescore = Math.floor(lovescore) + 1; // 1-100
 
-if(lovescore > 70) {
+if (lovescore > 70) {
   alert("Your love score is " + lovescore + "%" + " You're love is everlasting.");
 } 
-if(lovescore > 30 &&  lovescore <= 70){
+if (lovescore > 30 &&  lovescore <= 70){
   alert("Your love score is " + lovescore + "%");  
 }
 if (lovescore <= 30){
   alert("Your love score is " + lovescore + "%" + " You go together like oil and water.");  
 }
+
+
+//////////////////////////////// Second Iteration  /////////////////////////////////////////////////
+
+
+prompt("What is your name?");
+prompt("What is their name?");
+
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1;
+alert("Your love score is " + lovescore + "%");
